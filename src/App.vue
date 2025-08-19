b7a6db9a77bc400b01df8f67bf8766b3dc525faf
Alan Garny <agarny@hellix.com>
2025-08-20 10:57:43 +1200

    Removed some of the boiler plate.

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,7 @@ import TheWelcome from './components/TheWelcome.vue'
 </script>
 
 <template>
+  <!-- ---ABI---
   <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
@@ -15,6 +16,7 @@ import TheWelcome from './components/TheWelcome.vue'
   <main>
     <TheWelcome />
   </main>
+  -->
 </template>
 
 <style scoped>
