993c0eebf1bce8a8ff4531dab4cdf1bc2de8ffde
David Nickerson <david.nickerson@gmail.com>
2025-08-25 12:49:37 +1200

    Making external links clear, increase font size for intro text block.

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import * as VueRouter from "vue-router";
 import App from "./App.vue";
 import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
+import externalLink from './directives/external-link'
 
 const routes = [{ path: "/", component: App }];
 const router = VueRouter.createRouter({
@@ -16,4 +17,5 @@ const app = createApp(App);
 
 app.use(router);
 app.use(ElementPlus);
+app.directive('external', externalLink)
 app.mount("#app");
